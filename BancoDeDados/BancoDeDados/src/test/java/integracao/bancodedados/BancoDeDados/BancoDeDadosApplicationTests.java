4bdbeb219e624beeaa9a470a9f741f569fde0388
julia <juliazoratti@gmail.com>
2021-04-08 14:40:47 -0300

    testes unitarios: model, repository, controller

---
+++ BancoDeDados/BancoDeDados/src/test/java/integracao/bancodedados/BancoDeDados/BancoDeDadosApplicationTests.java
@@ -0,0 +1,12 @@
+package integracao.bancodedados.BancoDeDados;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeDadosApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
---
+++ BancoDeDados/BancoDeDados/src/test/java/integracao/bancodedados/BancoDeDados/contatos/ContatosRepositoryIntegrationTest.java
@@ -0,0 +1,56 @@
+package integracao.bancodedados.BancoDeDados.contatos;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import integracao.bancodedados.BancoDeDados.model.ContatoModel;
+import integracao.bancodedados.BancoDeDados.repository.ContatoRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ContatosRepositoryIntegrationTest {
+
+		private ContatoModel contato;
+		
+		@Autowired
+		private ContatoRepository contatoRepository;
+		
+		String message= "Employee ID is null";
+		
+		@Before
+		public void start() {
+			contato = new ContatoModel("Gabriel", "011y", "9xxxxxxx9");
+		}
+		
+		//regra, não aceita exceções, é a maneira de especificar
+		//que a execução de um teste lançará uma exceção
+		//Verifica se o @NotEmpty está funcionando
+
+		
+		@Test (expected = RuntimeException.class)
+		public void salvarComTelNulo() throws Exception {
+			
+			
+			contato.setTelefone(null);
+			contatoRepository.save(contato);
+		}
+		
+		@Test (expected = RuntimeException.class)
+		public void salvarComDddNulo() throws Exception {
+			
+			
+			contato.setDdd(null);
+			contatoRepository.save(contato);
+		}
+		
+		@Test (expected = RuntimeException.class)
+		public void salvarComNomeNulo() throws Exception {
+			
+			contato.setNome(null);
+			contatoRepository.save(contato);
+		}
+
+}
